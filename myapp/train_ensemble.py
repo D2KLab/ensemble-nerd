6f375165566b2b3aca3aa4d43d45e4df3de09d9e
Lorenzo Canale <s230016@studenti.polito.it>
2018-04-04 12:42:53 +0200

    update

---
+++ myapp/train_ensemble.py
@@ -0,0 +1,26 @@
+import subprocess
+import sys
+
+base = sys.argv[1]
+cmd_features = ['getDatasetFeatures.py', base]
+
+try:
+    lang = sys.argv[2]
+    cmd_features.append('--lang')
+    cmd_features.append(lang)
+except:
+    pass
+
+cmd_train_ENTTR = ['ENTTR_train.py', base]
+cmd_train_ENND = ['ENND_train.py', base]
+cmd_output = ['getOutput.py',base,base]
+cmd_evaluate = ['evaluate_dataset.py',base]
+
+commands = [cmd_features,cmd_train_ENTTR,cmd_train_ENND,cmd_output,cmd_evaluate]
+
+for cmd in commands:
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    for line in p.stdout:
+        print (line)
+    p.wait()
+    print (p.returncode)
